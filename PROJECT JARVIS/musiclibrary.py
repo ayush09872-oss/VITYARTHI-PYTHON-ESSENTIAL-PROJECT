602a86664964ad349565d4febb5c79c2837f1189
AYUSH SHUKLA <ayush.25bai10168@vitbhopal.ac.in>
2025-11-23 16:13:12 +0530

    Add files via upload

---
+++ PROJECT JARVIS/musiclibrary.py
@@ -0,0 +1,11 @@
+music ={
+    "yalgaar": "https://open.spotify.com/album/6JoCfnx3ytn78UHeEwFS5I?si=fe38957070fd4101",
+    "daru badnam": "https://open.spotify.com/album/5KLNI7nhEUDcxF0sGM0f0w?si=918bc13d4c884f2e",
+    "khushi jab bhi teri": "https://open.spotify.com/album/2VZlJpjJ4PD4DnVf2yPphq?si=e1b64b2bb13b43a4",
+    "daru party": "https://open.spotify.com/album/7q69B8KDb7cglbfxQQzk8u?si=0d7aab75f6254a5c",
+    "bones": "https://open.spotify.com/album/6yiXkzHvC0OTmhfDQOEWtS?si=51d8a9f009144478",
+    "toofan": "https://open.spotify.com/album/62afs3y06fy6gZqMUASF9I?si=6e673fd647dd4c1a"
+
+
+
+}
